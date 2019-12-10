6f7d159cb9fb9c737708d05e963b50d9de3712f8
yiluheihei <yiluheihei@gmail.com>
2019-12-10 17:05:27 +0800

    2010年到2018年的IF

--- app.R
+++ app.R
@@ -1,22 +1,39 @@
 library(shiny)
 library(DT)
 library(dplyr)
+library(purrr)
+library(shinyWidgets)
 
 ui <- fluidPage(
-  selectInput("year", label = "请选择年份", choices = 2019),
+  numericRangeInput('start_end', '请选择起止年份', value = c(2010, 2018)),
   dataTableOutput("jcr_table")
 )
 
 server <- function(input, output, session) {
+  #' load journal citation reports
+  #' 
+  #' @param year integer, the JCR year to load
+  #' @return a two length data frame, contains 'Full Journal Title' and "Journal
+  #' Impact Factor'
+  read_jcr <- function(year, n_max) {
+    file <- file.path("data", paste0(year,".xlsx"))
+    var_names <- c("rank", "journal", "total_cites", "bad",
+      "impact_f", "eigenfactor_score")
+    jcr <- read_xlsx(file, skip = 3, col_names = var_names) %>%
+      distinct() %>% 
+      select(journal, impact_f) %>% 
+      slice((-n() + 1):-n()) %>% 
+      mutate(impact_f = ifelse(impact_f == "Not Available", NA, impact_f))
+    
+    jcr
+  }
+  
   jcr <- reactive({
-    readxl::read_xlsx(file.path("data", paste0(input$year,".xlsx")),
-      skip = 2) %>% 
-      select(one_of(c("Rank", "Full Journal Title", "Total Cites",
-        "Journal Impact Factor", "Eigenfactor Score"
-        )))
-      
+    purrr::map(input$start_end[2]:input$start_end[1], read_jcr) %>% 
+      reduce(full_join, by = "journal") %>% 
+      set_names(c("journal", input$start_end[2]:input$start_end[1]))
   })
-
+  
   output$jcr_table <- renderDT(
     jcr(),
     options = list(pageLength = 50))
